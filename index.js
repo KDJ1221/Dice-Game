bd8705e6103a18728f224484065ef6050933ac79
KDJ1221 <kimmikojames@gmail.com>
2020-09-11 15:50:06 -0700

    Created win conditions

--- index.js
+++ index.js
@@ -12,21 +12,35 @@ const game_message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
+// Main Game Setup
 rollBtn.addEventListener("click", function(){
     const randomNumber = Math.floor(Math.random() * 6) + 1
-    console.log(randomNumber)
 
     if (player1Turn){
+        playerOneScore += randomNumber
+        player1Scoreboard.textContent = playerOneScore
         player1Dice.textContent = randomNumber
         player1Dice.classList.remove("active")
         player2Dice.classList.add("active")
         game_message.textContent = "Player 2 Turn"
     } else {
+        playerTwoScore += randomNumber
+        player2Scoreboard.textContent = playerTwoScore
         player2Dice.textContent = randomNumber
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
         game_message.textContent = "Player 1 Turn"
         
+    }
+
+    if(playerOneScore >= 20) {
+        game_message.textContent = "Player 1 has won!"
+        rollBtn.style.display = "none"
+        resetBtn.style.display = "block"
+    } else if(playerTwoScore >= 20){
+        game_message.textContent = "Player 2 has won!"
+        rollBtn.style.display = "none"
+        resetBtn.style.display = "block"
     }
 
     player1Turn = !player1Turn
