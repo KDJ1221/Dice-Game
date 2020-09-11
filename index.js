ba27e7ad52070113ce964bf7bc4a8a6083b2f7c8
KDJ1221 <kimmikojames@gmail.com>
2020-09-11 16:40:52 -0700

    Created game reset

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const game_message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
+function displayButtons () {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
 // Main Game Setup
 rollBtn.addEventListener("click", function(){
     const randomNumber = Math.floor(Math.random() * 6) + 1
@@ -34,14 +39,37 @@ rollBtn.addEventListener("click", function(){
     }
 
     if(playerOneScore >= 20) {
-        game_message.textContent = "Player 1 has won!"
-        rollBtn.style.display = "none"
-        resetBtn.style.display = "block"
+        game_message.textContent = "Player 1 has won! 🥳"
+        displayButtons()
     } else if(playerTwoScore >= 20){
-        game_message.textContent = "Player 2 has won!"
-        rollBtn.style.display = "none"
-        resetBtn.style.display = "block"
+        game_message.textContent = "Player 2 has won! 🥳"
+        displayButtons()
     }
 
     player1Turn = !player1Turn
 })
+// Reset Button Setup
+resetBtn.addEventListener("click", function(){
+    reset()
+})
+
+function reset() {
+    game_message.textContent = "Player 1 Turn"
+
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+
+    player2Dice.textContent = "-"
+    player1Dice.textContent = "-"
+
+    playerOneScore = 0
+    playerTwoScore = 0
+
+    player1Turn = true
+
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"   
+
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
